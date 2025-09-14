ef22443c2be92d5c5a86d8bdd491f38a6f2967c8
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-09-14 10:18:02 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,65 @@
+const LANG_EN_CAMERA = {
+    PHOTO: "Photo",
+    VIDEO: "Video",
+    GALLERY: "Gallery",
+    ENABLE_CAMERA_PROMPT: "Camera access needed to take photos",
+    ENABLE_CAMERA_BTN: "Enable Camera",
+    FLIP_CAMERA_TITLE: "Flip Camera",
+    TAKE_PHOTO_TITLE: "Take Photo",
+    FLASH_TITLE: "Flash"
+};
+
+const LANG_JP_CAMERA = {
+    PHOTO: "写真",
+    VIDEO: "ビデオ",
+    GALLERY: "ギャラリー",
+    ENABLE_CAMERA_PROMPT: "写真を撮るにはカメラへのアクセスが必要です",
+    ENABLE_CAMERA_BTN: "カメラを有効にする",
+    FLIP_CAMERA_TITLE: "カメラを反転",
+    TAKE_PHOTO_TITLE: "写真を撮る",
+    FLASH_TITLE: "フラッシュ"
+};
+
+const LANG_DE_CAMERA = {
+    PHOTO: "Foto",
+    VIDEO: "Video",
+    GALLERY: "Galerie",
+    ENABLE_CAMERA_PROMPT: "Kamerazugriff zum Aufnehmen von Fotos erforderlich",
+    ENABLE_CAMERA_BTN: "Kamera aktivieren",
+    FLIP_CAMERA_TITLE: "Kamera wechseln",
+    TAKE_PHOTO_TITLE: "Foto aufnehmen",
+    FLASH_TITLE: "Blitz"
+};
+
+const LANG_ES_CAMERA = {
+    PHOTO: "Foto",
+    VIDEO: "Video",
+    GALLERY: "Galería",
+    ENABLE_CAMERA_PROMPT: "Se necesita acceso a la cámara para tomar fotos",
+    ENABLE_CAMERA_BTN: "Habilitar cámara",
+    FLIP_CAMERA_TITLE: "Girar cámara",
+    TAKE_PHOTO_TITLE: "Tomar foto",
+    FLASH_TITLE: "Flash"
+};
+
+const LANG_KO_CAMERA = {
+    PHOTO: "사진",
+    VIDEO: "비디오",
+    GALLERY: "갤러리",
+    ENABLE_CAMERA_PROMPT: "사진을 찍으려면 카메라 접근 권한이 필요합니다",
+    ENABLE_CAMERA_BTN: "카메라 활성화",
+    FLIP_CAMERA_TITLE: "카메라 전환",
+    TAKE_PHOTO_TITLE: "사진 찍기",
+    FLASH_TITLE: "플래시"
+};
+
+const LANG_ZH_CAMERA = {
+    PHOTO: "照片",
+    VIDEO: "视频",
+    GALLERY: "相册",
+    ENABLE_CAMERA_PROMPT: "需要相机权限才能拍照",
+    ENABLE_CAMERA_BTN: "启用相机",
+    FLIP_CAMERA_TITLE: "翻转相机",
+    TAKE_PHOTO_TITLE: "拍照",
+    FLASH_TITLE: "闪光灯"
+};
